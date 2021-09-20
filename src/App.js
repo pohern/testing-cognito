803ff61acc14a2c982510bb2adf95681f4e3244d
pohern <ohern_p@yahoo.com>
2021-09-20 11:59:03 -0500

    Implement context to access data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Account } from './components/Accounts'
 import Signup from './components/SignUp'
 import Login from './components/Login'
 
 function App() {
   return (
-    <div>
+    <Account>
       <Signup />
       <Login />
-    </div>
+    </Account>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,21 @@
-import React, { useState } from "react";
-import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
-import UserPool from "../UserPool";
+import React, { useState, useContext } from "react";
+import { AccountContext } from "./Accounts";
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const { authenticate } = useContext(AccountContext);
+
   const onSubmit = (e) => {
     e.preventDefault();
-    const user = new CognitoUser({
-        Username: email,
-        Pool: UserPool,
-    })
-    const authDetails = new AuthenticationDetails({
-        Username: email,
-        Password: password
-    })
-
-    user.authenticateUser(authDetails, {
-        onSuccess: data => {
-            console.log('onSuccess: ', data)
-        },
-        onFailure: err => {
-            console.log('onFailure: ', err)
-        },
-        newPasswordRequired: data => {
-            console.log('newPasswordRequired: ', data)
-        }
-    })
+    authenticate(email, password)
+      .then((data) => {
+        console.log("Logged In!", data);
+      })
+      .catch((err) => {
+        console.log("Failed to login!", err);
+      });
   };
   return (
     <div>
